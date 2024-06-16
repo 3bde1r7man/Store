a3f92c26eaa125021d58527262fddeecd8bf3378
Abdelrhman Mostafa <abdelrhman.m.hessain@gmail.com>
2024-06-16 23:20:00 +0300

    Add project files.

---
+++ Store/Data/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Store.Models;
+
+namespace Store.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string projectRootDirectory = Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.FullName;
+            string databaseDirectory = Path.Combine(projectRootDirectory, "db");
+
+            // Ensure the 'dp' directory exists
+            if (!Directory.Exists(databaseDirectory))
+            {
+                Directory.CreateDirectory(databaseDirectory);
+            }
+
+            string databasePath = Path.Combine(databaseDirectory, "StoreDB.db");
+            optionsBuilder.UseSqlite($"Data Source={databasePath}");
+        }
+
+        public void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new UserConfiguration());
+        }
+    }
+
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            
+        }
+    }
+}
