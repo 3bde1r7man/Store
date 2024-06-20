aced9aacfc39659e4c9b1c8eecc2c3a03fd2d631
Abdelrhman Mostafa <abdelrhman.m.hessain@gmail.com>
2024-06-20 20:04:19 +0300

    #Added Edit to Order To Update Status of the order #Update UI for Create, edit, delete, details for product, category, order #Fix Order History to get the user orders only #Added Get All Orders For Admin

--- Store/Controllers/OrdersController.cs
+++ Store/Controllers/OrdersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -23,8 +24,23 @@ namespace Store.Controllers
         // GET: Orders
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Orders.Include(o => o.User);
-            return View(await applicationDbContext.ToListAsync());
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == User.Identity.Name);
+            var UserOrders = _context.Orders
+                .Include(o => o.User)
+                .Where(o => o.UserId == user.Id);
+
+            return View(await UserOrders.ToListAsync());
+
+        }
+
+        // GET: AllOrders
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> AllOrders()
+        {
+            var orders = _context.Orders
+                .Include(o => o.User);
+
+            return View(await orders.ToListAsync());
         }
 
         // GET: Orders/Details/5
@@ -43,19 +59,67 @@ namespace Store.Controllers
                 .Include(op => op.Product)
                 .Where(op => op.OrderId == id)
                 .ToListAsync();
-
-            if (order == null)
-            {
-                return NotFound();
-            }
-
-            var model = new DetailsModel(_context)
+            var moder = new DetailsModel(_context)
             {
                 Order = order,
                 OrderProducts = orderProducts
             };
+            return View(moder);
+        }
+       
 
-            return View(model);
+        // GET: Orders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", order.UserId);
+            order.User = _context.Users.Find(order.UserId);
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,Date,Status,TotalPrice")] Order order)
+        {
+            if (id != order.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", order.UserId);
+            return View(order);
         }
 
 
